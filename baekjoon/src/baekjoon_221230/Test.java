9aa00531a0fe79b8a086ae52e64aaf90ebe5cece
jonghwi_park <whitejack.paper28@gmail.com>
2023-01-04 03:06:02 +0900

    <baekjoon>
    1. added problem No.2606 (virus)

---
+++ baekjoon/src/baekjoon_221230/Test.java
@@ -0,0 +1,12 @@
+package baekjoon_221230;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int num = 150000;
+		
+		int result = num * (1 - 5 / 100);
+		System.out.println(result);
+		System.out.println(1 - 5 / 100);
+	}
+}

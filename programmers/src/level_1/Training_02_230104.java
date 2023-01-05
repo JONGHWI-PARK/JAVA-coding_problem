9d8989da784e286ef2d09f341eb0a7d14980c4ed
jonghwi_park <whitejack.paper28@gmail.com>
2023-01-05 13:43:08 +0900

    <programmers>
    1. level_0
    
    - added coding problems (2EA)
    
      - 배열의 유사도
      - 중앙값 구하기
    
    2. added level_1
    
    - 약수의 합
    - 자릿수 더하기

---
+++ programmers/src/level_1/Training_02_230104.java
@@ -0,0 +1,54 @@
+/*
+자릿수 더하기
+
+문제 설명
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+제한사항
+N의 범위 : 100,000,000 이하의 자연수
+
+입출력 예
+N		answer
+123		6
+987		24
+
+입출력 예 설명
+
+입출력 예 #1
+문제의 예시와 같습니다.
+
+입출력 예 #2
+9 + 8 + 7 = 24이므로 24를 return 하면 됩니다.
+ */
+
+package level_1;
+
+public class Training_02_230104 {
+
+	public static void main(String[] args) {
+		NumSum ns = new NumSum();
+		
+		int n1 = 123;
+		int n2 = 987;
+		
+		System.out.println(ns.solution(n1));
+		System.out.println(ns.solution(n2));
+	}
+
+}
+
+class NumSum {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0) {
+            answer += (n%10); 
+            // 10으로 나누면 나머지가 각 자리수가 된다.
+            n /= 10;
+            // 각 자리수를 추출하고 나면 10으로 나눈 몫으로 모든 자리의 수를 추출할 때까지 반복문을 진행한다.
+        }
+
+        return answer;
+    }
+}

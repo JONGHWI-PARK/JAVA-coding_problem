213a3053488484cbed5912cdadf30d7bc4689e29
jonghwi_park <whitejack.paper28@gmail.com>
2023-01-11 20:07:16 +0900

    <programmers>
    1. added coding problems
    
    i) 230105
    level_0
    
    - 문자열 안에 문자열
    - 순서쌍의 개수
    
    ii) 230106
    level_1
    
    - 평균 구하기

---
+++ programmers/src/level_1/Training_01_230106.java
@@ -0,0 +1,46 @@
+/*
+평균 구하기
+
+문제 설명
+정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+제한사항
+arr은 길이 1 이상, 100 이하인 배열입니다.
+arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+입출력 예
+arr			return
+[1,2,3,4]	2.5
+[5,5]		5
+ */
+
+package level_1;
+
+public class Training_01_230106 {
+
+	public static void main(String[] args) {
+		ArrAvg aa = new ArrAvg();
+		
+		int[] arr1 = {1, 2, 3, 4};
+		int[] arr2 = {5, 5};
+		
+		System.out.println(aa.solution(arr1));
+		System.out.println(aa.solution(arr2));
+	}
+
+}
+
+class ArrAvg {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        double sum = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        answer = sum/arr.length;
+        
+        return answer;
+    }
+}

caf3775205f3e6592dd42746c50558a20c8fa96b
jonghwi_park <whitejack.paper28@gmail.com>
2023-02-01 23:38:33 +0900

    <baekjoon>
    added coding problems (2EA)
    
    1. 230115
    
    - 그룹 단어 체커 (No.1316)
    
    2. 230117
    
    - 알파벳 찾기 (No.10809)

---
+++ baekjoon/src/baekjoon_230117/Test.java
@@ -0,0 +1,16 @@
+package baekjoon_230117;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String[] strArray = {"Hello", " ", "Java", " ", "Programming"};
+		String strArrayToString  = Arrays.toString(strArray);
+
+		System.out.println(strArrayToString);
+	}
+
+}
+
